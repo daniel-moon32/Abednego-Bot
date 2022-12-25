6f170ad469d9c8fb8305a8586c65964a842103fd
daniel-moon32 <dm32@illinois.edu>
2022-12-24 19:15:41 -0600

    finished bot

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, VoiceConnection } = require('@discordjs/voice');
+const { join } = require('node:path');
 require('dotenv').config();
 
 // import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const player = createAudioPlayer();
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -16,9 +19,32 @@ client.once(Events.ClientReady, c => {
 console.log(process.env.DISCORD_TOKEN)
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-	if (oldState.member.user.bot) return;
-	console.log(newState)
-})
+	// console.log(newState)
+	if (newState.member.id === process.env.USER_ID && oldState.channelId === null) {
+		console.log(__dirname)
+		const happy_birthday = createAudioResource(join(__dirname, 'happy_birthday.mp3'));
+		console.log('User joined voice')
+		const connection = joinVoiceChannel({
+			channelId: newState.channelId,
+			guildId: newState.guild.id,
+			adapterCreator: newState.guild.voiceAdapterCreator,
+		});
+		console.log(happy_birthday)
+		console.log('get audio file')
+
+		setTimeout(() => {
+
+			player.play(happy_birthday);
+			const subscription = connection.subscribe(player);
+			console.log('play audio file')
+			if (subscription) {
+				setTimeout(() => subscription.unsubscribe(), 5_000);
+			}
+			setTimeout(() => connection.destroy(), 5_000);
+		}, 5_000);
+	}
+});
+
 
 client.login(process.env.DISCORD_TOKEN);
 
